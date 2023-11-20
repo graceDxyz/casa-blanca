83f8e815a354b4cdf517bac2c6e76c24bbf7a4b0
Brixter Porras <brixterporras@gmail.com>
2023-11-20 22:57:43 +0800

    fix: on 404 page

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,28 +1,46 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
+const rewriteSlashToIndexHtml = () => {
+  return {
+    name: 'rewrite-slash-to-index-html',
+    apply: 'serve',
+    enforce: 'post',
+    configureServer(server) {
+      // rewrite / as index.html
+      server.middlewares.use('/', (req, _, next) => {
+        if (req.url === '/') {
+          req.url = '/index.html';
+        }
+        next();
+      });
+    },
+  };
+};
 
 const defaultConfig = {
-  plugins: [react()],
+  appType: 'mpa', // disable history fallback
+  plugins: [react(), rewriteSlashToIndexHtml()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
 };
 
 export default defineConfig(({ command, mode }) => {
-  if (command === "serve") {
-    const isDev = mode === "development";
+  if (command === 'serve') {
+    const isDev = mode === 'development';
 
     return {
       ...defaultConfig,
       server: {
         proxy: {
-          "/api": {
+          '/api': {
             target: isDev
-              ? "http://localhost:5000"
-              : "https://agri-map-58go.onrender.com",
+              ? 'http://localhost:5000'
+              : 'https://agri-map-58go.onrender.com',
             // changeOrigin: isDev,
             // secure: !isDev,
           },
