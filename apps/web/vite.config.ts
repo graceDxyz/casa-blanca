053189ec2f85bf9bfca4bc5db037332a3dd380c2
Brixter Porras <brixterporras@gmail.com>
2023-11-20 23:00:20 +0800

    feat: added vervel.json

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -2,26 +2,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
-const rewriteSlashToIndexHtml = () => {
-  return {
-    name: 'rewrite-slash-to-index-html',
-    apply: 'serve',
-    enforce: 'post',
-    configureServer(server) {
-      // rewrite / as index.html
-      server.middlewares.use('/', (req, _, next) => {
-        if (req.url === '/') {
-          req.url = '/index.html';
-        }
-        next();
-      });
-    },
-  };
-};
-
 const defaultConfig = {
-  appType: 'mpa', // disable history fallback
-  plugins: [react(), rewriteSlashToIndexHtml()],
+  plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
